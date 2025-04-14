1f9a3fab6d1bf0104cfe52bbec80c21f40c20019
maruseevvlad <maruseev.vlad@mail.ru>
2025-04-14 18:33:41 +0300

    добавление в справочную информацию выбор формата и параметров содержащих пути до сравниваемых файлов

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,17 +5,34 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+import java.io.File;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-class App implements Runnable {
+class App implements Callable<Integer> {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private Path pathToFile1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private Path pathToFile2;
+
+    @Option(names = { "-f", "--format" },
+            paramLabel = "format",
+            description = "output format [default: stylish]",
+            defaultValue = "stylish")
+    private String format;
 
     @Override
-    public void run() {
-        // your business logic goes here...
+    public Integer call() throws Exception {
         System.out.println("Application started.");
+        return 0;
     }
 
     public static void main(String[] args) {
