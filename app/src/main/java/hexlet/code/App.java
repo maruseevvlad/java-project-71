3eaa7e46cfb895b73116e905a877bd837af6c58a
maruseevvlad <maruseev.vlad@mail.ru>
2025-04-14 17:47:42 +0300

    создание логики консольного приложения с помощью picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+class App implements Runnable {
+
+    @Override
+    public void run() {
+        // your business logic goes here...
+        System.out.println("Application started.");
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
